e175d19a94e7fe87dbca5275a824954f0b306875
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2025-06-15 17:33:42 +0200

    authors and such

---
+++ authors.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+from script_paths import CLOC_DIR, MAAT_DIR, MERGE_DIR, TRANSFORM_DIR 
+
+# ALERT: ENSURE PYTHON3-BRANCH IS CHECKED OUT FROM THE MAAT-SCRIPTS
+# ALERT: YOU MAY WANT TO REMOVE NODE_MODULES FOR CLOC
+
+
+dir = input("Please give the directory to analyze: ")
+os.chdir(dir)
+name = input("Please give an abbreviation for the file(s): ");
+sinceDate = input("Please give the date since when the log should be analyzed (default 2021-08-01): ")
+if (sinceDate == ""): sinceDate = "2021-08-01"
+
+create_log = f"git log --all --numstat --date=short --pretty=format:--%h--%ad--%aN --no-renames --after={sinceDate} -- src/Compilers/CSharp/Test > git_log_" + name + ".txt"
+subprocess.check_output(create_log, shell=True)
+print("Created git log")
+
+run_maat = rf"java -jar {MAAT_DIR}\code-maat-1.0.4-standalone.jar -l git_log_{name}.txt -c git2 -a authors > authors_{name}.csv"
+subprocess.check_output(run_maat, shell=True)
+print("Ran Codemaat")
---
+++ module_coupling.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+from script_paths import MAAT_DIR
+
+dir = input("Please give the directory to analyze: ")
+os.chdir(dir)
+sinceDate = input("Please give the date the git log has to be analyzed from (default 2020-01-01): ")
+module_overview = input("Please give the name of the module overview file: ")
+name = input("Please give an abbreviation for the file(s): ");
+
+print("Creating git log")
+create_log = f"git log --all --numstat --date=short --pretty=format:--%h--%ad--%aN --no-renames --after={sinceDate} > git_log_" + name + ".txt"
+print("Executing: ", create_log)
+subprocess.check_output(create_log, shell=True)
+print("Created git log")
+
+print("Running Codemaat")
+run_maat = rf"java -jar {MAAT_DIR}\code-maat-1.0.4-standalone.jar -l git_log_{name}.txt -c git2 -a coupling -g {module_overview} --min-coupling 20 > module_coupling_{name}.csv"
+subprocess.check_output(run_maat, shell=True)
+print(f"Ran Codemaat. Please check 'coupling_{name}.csv'.")
